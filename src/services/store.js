ed610dab2968d31dcd22c17aa110c18371bb54d2
Coder-AlokRaj <alokraj.ias3@gmail.com>
2023-07-27 10:32:30 +0530

    first commit

---
+++ src/services/store.js
@@ -0,0 +1,17 @@
+import { configureStore } from "@reduxjs/toolkit";
+
+import { articleApi } from "./article";
+
+// export const store = configureStore({
+//     reducer: {
+//         [articleApi.reducerPath]: articleApi.reducer,
+//     },
+//     middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(articleApi.middleware)
+// })
+
+export const store = configureStore({
+  reducer: { [articleApi.reducerPath]: articleApi.reducer },
+  middleware: (getDefaultMiddleware) => [
+    ...getDefaultMiddleware().concat(articleApi.middleware),
+  ],
+});
